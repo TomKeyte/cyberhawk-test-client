d05e301f9c61892e97fb17a8b55c4daa17e11c6d
thomaskeyte <tom.keyte@merj.com>
2020-11-13 11:58:07 +0000

    Configure AWS deployment with vue cli plugin

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,28 @@ module.exports = {
                 args[0].title = "CyberHawk Turbine Monitor";
                 return args;
             })
+    },
+
+    pluginOptions: {
+      s3Deploy: {
+        registry: undefined,
+        awsProfile: 'default',
+        overrideEndpoint: true,
+        endpoint: 's3.eu-west-2.amazonaws.com',
+        region: 'eu-west-2',
+        bucket: 'cyberhawkclient',
+        createBucket: false,
+        staticHosting: true,
+        staticIndexPage: 'index.html',
+        staticErrorPage: 'index.html',
+        assetPath: 'dist',
+        assetMatch: '**',
+        deployPath: '/',
+        acl: 'public-read',
+        pwa: false,
+        enableCloudfront: false,
+        pluginVersion: '4.0.0-rc3',
+        uploadConcurrency: 5
+      }
     }
 }
